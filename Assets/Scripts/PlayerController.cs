e987ec43e6c5bd83776c86e6b1356c40d4a7aabe
SmirnovDD <studioindevelopment@gmail.com>
2018-09-18 09:49:38 +0300

    Finished character rotation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,20 +7,24 @@ public class PlayerController : MonoBehaviour {
 	public float moveSpeed = 10; //скорость, на которую умножается вектор движения с джостика
     public float maxSpeed = 10; //максимальная скорость движения rigidB
     public float rotateSpeed = 2; //скорость, на которую умножается вектор с джостика поворота
+    public float maxAngularVelocity = 20;
     public float rotationOffcet;
     public float torqueForce;
 
 	private Rigidbody2D rigidB;
 	private Transform thisTransform;
+    private float torque;
+    private float targetAngle; //угол, на который будет поворачиваться персонаж
 
-    private readonly VectorPid angularVelocityController = new VectorPid(33, 0, 0.1f);
-    private readonly VectorPid headingController = new VectorPid(26, 0, 0.1f);
+    private readonly VectorPid angleController = new VectorPid(33.7766f, 0, 0.2553191f);
+    private readonly VectorPid angularVelocityController = new VectorPid(9.244681f, 0, 0.06382979f);
 
     // Use this for initialization
     void Start ()
 	{
 		rigidB = GetComponent<Rigidbody2D>();
 		thisTransform = transform;
+        targetAngle = transform.eulerAngles.z;
 	}
 	
 	// Update is called once per frame
@@ -32,7 +36,8 @@ public class PlayerController : MonoBehaviour {
 
 	public void Movement()
 	{
-        Vector2 moveVector = new Vector2(CrossPlatformInputManager.GetAxisRaw("Horizontal"), CrossPlatformInputManager.GetAxisRaw("Vertical")) * moveSpeed;
+        //Vector2 moveVector = new Vector2(CrossPlatformInputManager.GetAxisRaw("Horizontal"), CrossPlatformInputManager.GetAxisRaw("Vertical")) * moveSpeed;
+        Vector2 moveVector = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * moveSpeed;
         Vector3 localVelocity = thisTransform.InverseTransformDirection(rigidB.velocity);
         Vector2 velocityChange = thisTransform.InverseTransformDirection(moveVector.x, moveVector.y, 0) - localVelocity;
 
@@ -46,28 +51,17 @@ public class PlayerController : MonoBehaviour {
     {
         Vector3 rotateVector = new Vector3(CrossPlatformInputManager.GetAxisRaw("RotateHorizontal"), CrossPlatformInputManager.GetAxisRaw("RotateVertical"), 0);
 
-        //if(rigidB.rotation < (Mathf.Atan2(rotateVector.y, rotateVector.x) * Mathf.Rad2Deg - 90) - rotationOffcet)
-        //{
-        //    rigidB.AddTorque(torqueForce);
-        //}
+        targetAngle = (Mathf.Atan2(rotateVector.y, rotateVector.x) * Mathf.Rad2Deg - 90) * rotateSpeed * Time.fixedDeltaTime;
 
-        var angularVelocityError = rigidB.angularVelocity * -1;
+        float angleError = Mathf.DeltaAngle(transform.eulerAngles.z, targetAngle);
+        float torqueCorrectionForAngle = angleController.Update(angleError, Time.fixedDeltaTime);
 
-        var angularVelocityCorrection = angularVelocityController.Update(angularVelocityError, Time.deltaTime);
+        rigidB.angularVelocity = Mathf.Clamp(rigidB.angularVelocity, -maxAngularVelocity, maxAngularVelocity);
 
-        rigidB.AddTorque(angularVelocityCorrection);
+        float angularVelocityError = -rigidB.angularVelocity;
+        float torqueCorrectionForAngularVelocity = angularVelocityController.Update(angularVelocityError, Time.fixedDeltaTime);
 
-        var desiredHeading = rotateVector;
-
-        Debug.DrawRay(transform.position, desiredHeading, Color.magenta);
-
-        var currentHeading = transform.up;
-        Debug.DrawRay(transform.position, currentHeading * 15, Color.blue);
-
-        var headingError = Vector3.Cross(currentHeading, desiredHeading);
-        var headingCorrection = headingController.Update(headingError.z, Time.deltaTime);
-
-        rigidB.AddTorque(headingCorrection);
-
+        torque = torqueCorrectionForAngle + torqueCorrectionForAngularVelocity;
+        rigidB.AddTorque(torque);
     }
 }
